a1c7bd13262ab27d7920db97cc9751da12e1d1f9
davith srun <srundavith12@gmail.com>
2024-06-14 16:07:09 +0700

    init

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'myaql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
